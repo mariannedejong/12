156386473a8c39139ea5e55c4ccc10fd0b16f17f
mariannedejong <89845904+mariannedejong@users.noreply.github.com>
2021-11-09 13:47:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy students")
+//dit is de eerste grote som
+/*()
+let groteSom1=1230941*1823794;
+console.log(groteSom1);
+let groteSom2=637263/54;
+console.log(groteSom2);
+*/
